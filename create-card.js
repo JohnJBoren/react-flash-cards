b259cb6bbf517033ee317a3054a49c7137e5aa2d
Jerome Chavez <jayrome12@yahoo.com>
2018-07-02 13:48:02 -0700

    Added a button element, submit, to the create-card component that resets the form when clicked

--- create-card.js
+++ create-card.js
@@ -3,14 +3,18 @@ import React from 'react'
 export default class CreateCard extends React.Component {
   render() {
     return (
-      <form>
+      <form className="create-flashcard">
+        <h2 className="header" for="create-flash-card">Create a Flash Card</h2>
         <div className="form-group">
           <label for="question-input">Question</label>
           <input type="text" className="form-control"/>
-          </div>
+        </div>
         <div className="form-group">
           <label for="answer-input">Answer</label>
           <input type="text" className="form-control"/>
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary button">Save</button>
         </div>
       </form>
   )}
