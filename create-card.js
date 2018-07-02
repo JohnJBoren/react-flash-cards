9b92dcf23f973ab8fcfd3f8db5370b60959554b8
Jerome Chavez <jayrome12@yahoo.com>
2018-07-02 15:32:51 -0700

    Added an event listener that would take user input and store it in React State

--- create-card.js
+++ create-card.js
@@ -1,17 +1,32 @@
 import React from 'react'
 
 export default class CreateCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(event) {
+    const data = new FormData(event.target)
+    const newCard = {}
+    for (let pair of data.entries()) {
+      newCard[pair[0]] = pair[1]
+    }
+    this.props.onInputChange(newCard)
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <form className="create-flashcard">
+      <form onSubmit= {this.handleSubmit} className="create-flashcard">
         <h2 className="header">Create a Flash Card</h2>
         <div className="form-group">
           <label>Question</label>
-          <input type="text" className="form-control"/>
+          <input type="text" name="question" className="form-control"/>
         </div>
         <div className="form-group">
           <label>Answer</label>
-          <input type="text" className="form-control"/>
+          <input type="text" name="answer" className="form-control"/>
         </div>
         <div className="form-group">
           <button type="submit" className="btn btn-primary button">Save</button>
--- react-flash-card.js
+++ react-flash-card.js
@@ -2,9 +2,23 @@ import React from 'react'
 import CreateCard from './create-card'
 
 export default class ReactFlashCards extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.state = {
+      cards: []
+    }
+  }
+
+  handleInputChange(userInput) {
+    const updateCards = [...this.state.cards, userInput]
+    this.setState({
+      cards: updateCards
+    })
+  }
   render() {
     return (
-      <CreateCard/>
+      <CreateCard onInputChange={ this.handleInputChange }/>
     )
   }
 }
