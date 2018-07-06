6453324392de2a708d7cf4e19e02e5564f532f86
Jerome Chavez <jayrome12@yahoo.com>
2018-07-06 13:14:01 -0700

    Refactored progress-bar.js from a class to function component because it is not binding methods or keeping local state #13

--- progress-bar.js
+++ progress-bar.js
@@ -1,18 +1,16 @@
 import React from 'react'
 
-export default class ProgressBar extends React.Component {
-  render() {
-    const { cards, index } = this.props
-    const progressLength = ((index + 1) / cards.length * 100)
-    const style = {
-      backgroundColor: 'rgb(44, 220, 55)',
-      width: `${progressLength}%`
+export default function ProgressBar(props) {
+  const { cards, index } = props
+  const progressLength = ((index + 1) / cards.length * 100)
+  const style = {
+    backgroundColor: 'rgb(44, 220, 55)',
+    width: `${progressLength}%`
 
-    }
-    return (
-      <div className="bar-progress card horizontal-margin">
-        <div className="bar-div" style={ style }></div>
-      </div>
-    )
   }
+  return (
+    <div className="bar-progress card horizontal-margin">
+      <div className="bar-div" style={ style }></div>
+    </div>
+  )
 }
