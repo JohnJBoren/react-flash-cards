4b9f29f6ff19bb0494fb6926751da1bfa7a8bf75
Jerome Chavez <jayrome12@yahoo.com>
2018-07-09 09:16:49 -0700

    Added a logo that matches the vibe of the app #15

--- nav.js
+++ nav.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 export default function Nav() {
   return (
-    <nav className="navbar mx-auto">
+    <nav className="navbar">
+      <i className="far fa-folder fa-4x text-white logo px-3"></i>
       <h4 className="text-white">React Flash Cards</h4>
-      <a className="nav-link text-white" href="#card-list">Cards</a>
-      <a className="nav-link text-white" href="#new-card">New</a>
-      <a className="nav-link text-white" href="#practice">Practice</a>
+      <a className="nav-link text-white mx-auto" href="#card-list">Cards</a>
+      <a className="nav-link text-white mx-auto" href="#new-card">New</a>
+      <a className="nav-link text-white mx-auto" href="#practice">Practice</a>
     </nav>
   )
 }
