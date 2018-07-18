cabf1444dbcd850c336293db2d9a9aa5d946e4c0
Jerome Chavez <jayrome12@yahoo.com>
2018-07-18 07:44:11 -0700

    Converted header element from h1 to span in order to better size and align with in-line elements

--- nav.js
+++ nav.js
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
     <nav className="navbar">
       <i className="far fa-folder fa-4x text-white logo px-3"></i>
-      <h4 className="text-white">React Flash Cards</h4>
+      <span style={ {'fontSize': '25px' } } className="text-white">React Flash Cards</span>
       <a className="nav-link text-white mx-auto" href="#card-list">Cards</a>
       <a className="nav-link text-white mx-auto" href="#new-card">New</a>
       <a className="nav-link text-white mx-auto" href="#practice">Practice</a>
